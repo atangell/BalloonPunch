fe4845cc6c32bd47aa8a4edb50928b2a4eb8e62e
atangell <atangell@my.centennialcollege.ca>
2016-10-03 00:54:40 -0400

    Third Commit

---
+++ Assets/_Scripts/BalloonController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonController : MonoBehaviour
+{
+
+    private Transform _transform;
+    // Use this for initialization
+    void Start()
+    {
+        this._transform = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._move();
+    }
+
+    private void _move()
+    {
+        this._transform.position = new Vector2(Mathf.Clamp(Input.mousePosition.x - 320f, -230f, 300f), -122f);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Island"))
+        {
+
+            Debug.Log("Hit");
+        }
+    }
+}
