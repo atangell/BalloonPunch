504171efc2002584231c72e572c98c9942575622
atangell <atangell@my.centennialcollege.ca>
2016-10-02 22:46:46 -0400

    Second Commit

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField]
+    private int _speed;
+    private Transform _transform;
+    public int Speed
+    {
+        get
+        {
+            return this._speed;
+        }
+        set
+        {
+            this._speed = value;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        this._transform = this.GetComponent<Transform>();
+        this._speed = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._move();
+        this._checkbounds();
+
+    }
+
+    private void _move()
+    {
+        Vector2 newPosition = this._transform.position;
+        newPosition.y -= this._speed;
+        this._transform.position = newPosition;
+    }
+
+    private void _checkbounds()
+    {
+        if (this._transform.position.y <= -576f)
+        {
+            this._reset();
+        }
+    }
+
+    private void _reset()
+    {
+        this._transform.position = new Vector2(0f, 675f);
+    }
+}
